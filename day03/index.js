f95540aedb196573a1a9115db5a9eafab70bd98e
Jason <jasonkwm13@gmail.com>
2023-12-24 01:19:38 +0800

    day03 (fix): input.txt was wrong

--- day03/index.js
+++ day03/index.js
@@ -4,12 +4,13 @@ const fs = require("fs");
 // [0, -1] [0, 0] [0, + 1]
 // [-1, 0] [+ 1, 0] [+ 1, + 1]
 
-function checkAdjacent(inputArray, col, row, maxColLen, maxRowLen) {
-    ignores = "0123456789.";
+function checkSymbol(inputArray, col, row, maxColLen, maxRowLen) {
+    ignores = "0123456789.\n";
     for (let i = -1; i < 2; i++) {
         for (let j = -1; j < 2; j++) {
             if (col + i === -1 || row + j === -1) continue;
             if (col + i >= maxColLen || row + j >= maxRowLen) continue;
+            // if (inputArray[col + i][row + j] === undefined) continue;
             if (ignores.includes(inputArray[col + i][row + j]) === false) {
                 console.log(inputArray[col + i][row + j]);
                 return true;
@@ -25,16 +26,17 @@ fs.readFile("input.txt", (err, input) => {
     const inputArray = input.toString().split("\n");
     const maxColLen = inputArray.length;
     const numbers = "0123456789";
-    for (let i = 1; i < maxColLen; i++) {
+    for (let i = 0; i < maxColLen; i++) {
         let curNumLen = 0;
         let isPart = false;
         const maxRowLen = inputArray[i].length;
         for (let j = 0; j < maxRowLen; j++) {
             if (numbers.includes(inputArray[i][j])) {
                 curNumLen++;
-                if (!isPart) isPart = checkAdjacent(inputArray, i, j, maxColLen, maxRowLen);
+                if (!isPart) isPart = checkSymbol(inputArray, i, j, maxColLen, maxRowLen);
             } else if (curNumLen !== 0 && isPart) {
                 answer += Number(inputArray[i].slice(j - curNumLen, j));
+                console.log(Number(inputArray[i].slice(j - curNumLen, j)));
                 curNumLen = 0;
                 isPart = false;
             } else {
